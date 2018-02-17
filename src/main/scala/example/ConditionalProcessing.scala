4ad18e782534972a8d4095375d7ebb78aa25df96
Thundzz <yacinenew@gmail.com>
2018-02-17 18:09:03 +0100

    try to make the processing block async

--- src/main/scala/example/ConditionalProcessing.scala
+++ src/main/scala/example/ConditionalProcessing.scala
@@ -2,22 +2,21 @@ package example
 
 import akka.NotUsed
 import akka.actor.ActorSystem
-import akka.stream.{ActorMaterializer, ClosedShape}
-import akka.stream.scaladsl.{Broadcast, Flow, GraphDSL, Merge, RunnableGraph, Sink, Source}
+import akka.stream.scaladsl.{Broadcast, Flow, GraphDSL, Merge, Sink, Source}
+import akka.stream.{ActorMaterializer, FlowShape}
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, ExecutionContextExecutor, Future}
+import scala.util.{Failure, Success}
 
 
 object ConditionalProcessing extends App {
 
+  implicit val ec: ExecutionContextExecutor = ExecutionContext.global
   implicit val system: ActorSystem = ActorSystem()
   implicit val mat: ActorMaterializer = ActorMaterializer()
 
-  val g = RunnableGraph.fromGraph(GraphDSL.create() { implicit builder: GraphDSL.Builder[NotUsed] =>
+  val flow = Flow.fromGraph(GraphDSL.create() { implicit builder: GraphDSL.Builder[NotUsed] =>
     import GraphDSL.Implicits._
-
-    val in = Source(1 to 100)
-    val out = Sink.foreach[Int](x => println(x))
 
     val isOdd = Flow[Int].filter(x => x % 2 != 0)
     val isEven = Flow[Int].filter(x => x % 2 == 0)
@@ -25,22 +24,30 @@ object ConditionalProcessing extends App {
     val bcast = builder.add(Broadcast[Int](2))
     val merge = builder.add(Merge[Int](2))
 
-    val f3 = Flow[Int].map(identity)
-
     val handleOdd = Flow[Int].mapAsync(10) {
       x =>
-        Thread.sleep(5000)
+        //Thread.sleep(500)
+        if (x == 51) {
+          throw new Exception("Problem, problem, a 51 was found !!!")
+        }
         Future.successful(x)
     }
 
     val handleEven = Flow[Int].map(identity)
+    bcast.out(0) ~> isOdd ~> handleOdd.async ~> merge.in(0)
+    bcast.out(1) ~> isEven ~> handleEven.async ~> merge.in(1)
 
-    in ~> bcast ~> isOdd ~> handleOdd ~> merge ~> f3 ~> out
-          bcast ~> isEven ~> handleEven ~> merge
-    ClosedShape
-
+    FlowShape(bcast.in, merge.out)
   })
+  val out = Sink.foreach[Int](x => println(x))
 
-  g.run()
-  system.terminate()
+  val future = Source(1 to 100).via(flow).runWith(out)
+
+  future.onComplete {
+    case Success(_) =>
+      println("finished.")
+      system.terminate()
+    case Failure(e) => println(e)
+      system.terminate()
+  }
 }
