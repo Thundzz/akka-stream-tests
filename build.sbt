9f76f99565faf783c6fa4ec7d13ee2a3b7916003
Yacine EL JARRARI <yacine.el-jarrari@atinternet.com>
2018-02-19 10:13:57 +0100

    test with kafka consumers

--- build.sbt
+++ build.sbt
@@ -12,6 +12,7 @@ lazy val root = (project in file(".")).
       scalaTest % Test,
       "com.typesafe.akka" %% "akka-stream" % "2.5.9",
       "com.typesafe.akka" %% "akka-stream-testkit" % "2.5.9" % Test,
-      "com.typesafe.akka" %% "akka-http"   % "10.1.0-RC1"
+      "com.typesafe.akka" %% "akka-http"   % "10.1.0-RC1",
+      "com.typesafe.akka" %% "akka-stream-kafka" % "0.19"
     )
   )
--- src/main/scala/example/ConditionalProcessing.scala
+++ src/main/scala/example/ConditionalProcessing.scala
@@ -2,18 +2,24 @@ package example
 
 import akka.NotUsed
 import akka.actor.ActorSystem
-import akka.stream.scaladsl.{Broadcast, Flow, GraphDSL, Merge, Sink, Source}
+import akka.kafka.scaladsl.Producer
+import akka.kafka.{ProducerMessage, ProducerSettings}
 import akka.stream.{ActorMaterializer, FlowShape}
+import akka.stream.scaladsl.{Broadcast, Flow, GraphDSL, Merge, Sink, Source}
+import org.apache.kafka.clients.producer.{ProducerConfig, ProducerRecord, RecordMetadata}
+import org.apache.kafka.common.TopicPartition
+import org.apache.kafka.common.serialization.{ByteArraySerializer, StringSerializer}
 
-import scala.concurrent.{ExecutionContext, ExecutionContextExecutor, Future}
+import scala.concurrent.{ExecutionContext, ExecutionContextExecutor}
 import scala.util.{Failure, Success}
-
 
 object ConditionalProcessing extends App {
 
   implicit val ec: ExecutionContextExecutor = ExecutionContext.global
   implicit val system: ActorSystem = ActorSystem()
   implicit val mat: ActorMaterializer = ActorMaterializer()
+
+  val KafkaIp = sys.env("KAFKA_IP")
 
   val flow = Flow.fromGraph(GraphDSL.create() { implicit builder: GraphDSL.Builder[NotUsed] =>
     import GraphDSL.Implicits._
@@ -23,22 +29,59 @@ object ConditionalProcessing extends App {
 
     val bcast = builder.add(Broadcast[Int](2))
     val merge = builder.add(Merge[Int](2))
+    val producerProps = Map[String, String](
+      ProducerConfig.CLIENT_ID_CONFIG -> "my-producer",
+      ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG -> classOf[ByteArraySerializer].getName,
+      ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG -> classOf[ByteArraySerializer].getName,
+      ProducerConfig.BATCH_SIZE_CONFIG -> "0"
+    )
 
-    val handleOdd = Flow[Int].mapAsync(10) {
-      x =>
-        //Thread.sleep(500)
-        if (x == 51) {
-          throw new Exception("Problem, problem, a 51 was found !!!")
-        }
-        Future.successful(x)
+    val producerSettings = ProducerSettings(system, new ByteArraySerializer, new StringSerializer)
+      .withBootstrapServers(KafkaIp)
+      .withProperties(producerProps)
+
+    val kafkaProducer = producerSettings.createKafkaProducer()
+
+    val handleOdd : Flow[Int, ProducerMessage.Message[Array[Byte], String, Int], NotUsed]  =
+      Flow[Int].map {
+        x =>
+          /*if (x == 51) {
+            throw new Exception("Problem, problem, intrusion in area 51!")
+          }*/
+          //Thread.sleep(1000)
+          x
+      }.map(
+        x =>
+          ProducerMessage.Message(
+            new ProducerRecord[Array[Byte], String]("some-dummy-topic", x.toString), x)
+      )
+
+    val result2Int = Flow[ProducerMessage.Result[Array[Byte], String, Int]].map {
+      x => x.message.record.value().toInt
     }
 
-    val handleEven = Flow[Int].map(identity)
-    bcast.out(0) ~> isOdd ~> handleOdd.async ~> merge.in(0)
-    bcast.out(1) ~> isEven ~> handleEven.async ~> merge.in(1)
+    val producerFlow: Flow[ProducerMessage.Message[Array[Byte], String, Int], ProducerMessage.Result[Array[Byte], String, Int], NotUsed] =
+      Producer.flow(producerSettings)
+    val fakeProducerFlow: Flow[ProducerMessage.Message[Array[Byte], String, Int], ProducerMessage.Result[Array[Byte], String, Int], NotUsed] =
+      Flow[ProducerMessage.Message[Array[Byte], String, Int]].map(
+        x=> {
+          val metadata = new RecordMetadata(new TopicPartition("", 1),1,1,1,1,1,1)
+          ProducerMessage.Result(metadata, x)
+        }
+      )
+
+    val handleEven = Flow[Int].map(x=> {
+      Thread.sleep(500)
+      x
+    })
+
+    bcast.out(0) ~> isOdd ~> handleOdd ~> fakeProducerFlow ~> result2Int ~> merge.in(0)
+    bcast.out(1) ~> isEven ~> handleEven ~> merge.in(1)
 
     FlowShape(bcast.in, merge.out)
   })
+
+
   val out = Sink.foreach[Int](x => println(x))
 
   val future = Source(1 to 100).via(flow).runWith(out)
